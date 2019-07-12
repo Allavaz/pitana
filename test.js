1ad375b0bd4cb0719fdadc6ea70acd9d39579420
David Zavalla <davidzavalla@gmail.com>
2019-07-12 08:32:46 -0300

    Delete test.js

--- test.js
+++
@@ -1,8 +0,0 @@
-const { DateTime, Interval } = require('luxon');
-
-da = DateTime.local()
-daa = da.plus({days: 1, minutes: 1})
-
-duration = Interval.fromDateTimes(da, daa).toDuration(['days', 'hours', 'minutes']).toObject()
-
-console.log(Math.ceil(0.1))
