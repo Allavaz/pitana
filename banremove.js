e3bafb1dc6ddf9c980a5c5ead84e55101c28ff48
David Zavalla <davidzavalla@gmail.com>
2019-11-27 22:28:07 -0300

    Parametrize ban reset time

--- banremove.js
+++ banremove.js
@@ -40,7 +40,7 @@ module.exports = async function(message, member) {
 		if (banlistitem.value.count === 0) {
 			exec(`schtasks /Delete /TN bansreset\\${memberid} /F`);
 		} else {
-			exec(`schtasks /Create /TN bansreset\\${memberid} /TR "node ${banresetpath} ${memberid}" /SD ${DateTime.fromISO(lastbanlog.startdate).plus(config.banreset).toFormat('LL\'/\'dd\'/\'yyyy')} /ST ${DateTime.fromISO(lastbanlog.startdate).plus({months: 2}).toFormat('HH\':\'mm')} /SC ONCE /F`);
+			exec(`schtasks /Create /TN bansreset\\${memberid} /TR "node ${banresetpath} ${memberid}" /SD ${DateTime.fromISO(lastbanlog.startdate).plus(config.banreset).toFormat('LL\'/\'dd\'/\'yyyy')} /ST ${DateTime.fromISO(lastbanlog.startdate).plus(config.banreset).toFormat('HH\':\'mm')} /SC ONCE /F`);
 		}
 		const removebanrembed = new Discord.RichEmbed()
 			.setTitle('Ban removido')
