4a9bde72c79e1382e6a58ea388ede4b80ea98474
David Zavalla <davidzavalla@gmail.com>
2019-11-28 02:37:06 -0300

    Set empty arguments to banranking

--- commands/banranking.js
+++ commands/banranking.js
@@ -3,6 +3,7 @@ const banranking = require('../banranking');
 module.exports = {
 	name: 'banranking',
 	description: 'Muestra los top 10 baneados',
+	usage: '',
 	execute(message) {
 		banranking(message);
 	}
