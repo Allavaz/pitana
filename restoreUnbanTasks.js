8898bd92893624027f52d995d50c9c51c7659197
David Zavalla <davidzavalla@gmail.com>
2021-11-27 19:39:02 -0300

    prettier

--- restoreUnbanTasks.js
+++ restoreUnbanTasks.js
@@ -40,23 +40,23 @@ const url = `mongodb://${encuser}:${encpw}@${config.dbhostname}:27017/?authMecan
 				}
 			])
 			.toArray();
-    let oldTasks = await db.collection("unbantasks").find({}).toArray();
+		let oldTasks = await db.collection("unbantasks").find({}).toArray();
 		newTasks = [];
 		docsFiltered = docs.filter(e => DateTime.fromISO(e.enddate) >= date);
 		docsFiltered.forEach(e => {
-      if (!oldTasks.find(e2 => e2.playerid === e._id)) {
-        newTasks.push({
-          playerid: e._id,
-          startdate: e.startdate,
-          enddate: e.enddate,
-          reason: e.reason
-        });
-      }
+			if (!oldTasks.find(e2 => e2.playerid === e._id)) {
+				newTasks.push({
+					playerid: e._id,
+					startdate: e.startdate,
+					enddate: e.enddate,
+					reason: e.reason
+				});
+			}
 		});
 		console.log(newTasks);
-    if (newTasks.length) {
-      await db.collection("unbantasks").insertMany(newTasks);
-    }
+		if (newTasks.length) {
+			await db.collection("unbantasks").insertMany(newTasks);
+		}
 	} catch (error) {
 		console.error(error);
 	}
