7898609180bd8dfee7fb4088c7cbe07907dcc8a4
David Zavalla <davidzavalla@gmail.com>
2021-11-28 17:40:22 -0300

    separate options for custom ban time

--- commands/ban.js
+++ commands/ban.js
@@ -17,81 +17,102 @@ module.exports = {
 		.addStringOption(option =>
 			option.setName("motivo").setDescription("Motivo de ban").setRequired(true)
 		)
+		.addIntegerOption(option =>
+			option
+				.setName("cantidad")
+				.setDescription("Tiempo personalizado: Cantidad de minutos/días/horas")
+		)
 		.addStringOption(option =>
 			option
-				.setName("duracion")
+				.setName("tipo")
 				.setDescription(
-					"Duración personalizada del ban. El formato de la duración es <número><m/h/d/perma>"
+					"Tiempo personalizado: días, minutos, horas o permanente"
 				)
+				.addChoice("minutos", "m")
+				.addChoice("horas", "h")
+				.addChoice("días", "d")
+				.addChoice("permanente", "perma")
 		),
 	permissions: config.adminroles,
 	channels: [config.channelid],
 	async execute(interaction) {
 		const member = interaction.options.getMember("jugador");
+		const tipo = interaction.options.getString("tipo");
+		const cantidad = interaction.options.getInteger("cantidad");
+		const motivo = interaction.options.getString("motivo");
 		if (member.roles.cache.get(config.mmbanroleid)) {
 			interaction.reply({
 				content: "El jugador ya se encuentra baneado.",
 				ephemeral: true
 			});
 		} else {
-			let durationObject;
-			if (interaction.options.getString("duracion")) {
-				const duration = interaction.options.getString("duracion").trim();
-				let durationType;
-				let durationNumber;
-				if (duration === "perma") {
-					durationObject = duration;
-				} else {
-					durationType = duration.substring(
-						duration.length - 1,
-						duration.length
-					);
-					durationNumber = parseInt(duration.substring(0, duration.length - 1));
-					switch (durationType) {
-						case "m":
-							durationObject = { minutes: durationNumber };
+			if (tipo && cantidad) {
+				let durationObject;
+				switch (tipo) {
+					case "perma":
+						return interaction.reply({
+							content: "No ingrese cantidad si ya eligió ban permanente.",
+							ephemeral: true
+						});
+					case "m":
+						if (cantidad < 2) {
+							return interaction.reply({
+								content: "Ingrese una cantidad de minutos mayor a 2.",
+								ephemeral: true
+							});
+						} else {
+							durationObject = { minutes: cantidad };
 							break;
-						case "h":
-							durationObject = { hours: durationNumber };
+						}
+					case "h":
+						if (cantidad < 1) {
+							return interaction.reply({
+								content: "Ingrese una cantidad de horas mayor a 0.",
+								ephemeral: true
+							});
+						} else {
+							durationObject = { hours: cantidad };
 							break;
-						case "d":
-							durationObject = { days: durationNumber };
+						}
+					case "d":
+						if (cantidad < 1) {
+							return interaction.reply({
+								content: "Ingrese una cantidad de días mayor a 0.",
+								ephemeral: true
+							});
+						} else {
+							durationObject = { days: cantidad };
 							break;
-						default:
-							break;
-					}
+						}
 				}
-				if (
-					(isNaN(durationNumber) && duration !== "perma") ||
-					durationObject === null
-				) {
-					interaction.reply({
-						content:
-							"Duración incorrecta. El formato de la duración es <número><m/h/d/perma> (minutos, horas y días, respectivamente)",
-						ephemeral: true
-					});
-				} else if (
-					duration !== "perma" &&
-					(durationNumber < 1 || (durationType === "m" && duration < 2))
-				) {
-					interaction.reply({
-						content: "Por favor ingresá un valor mayor a **un minuto**.",
-						ephemeral: true
-					});
-				} else {
-					await ban(
-						interaction,
-						member,
-						interaction.options.getString("motivo"),
-						durationObject
-					);
-				}
-			} else {
 				await ban(
 					interaction,
 					member,
-					interaction.options.getString("motivo")
+					motivo,
+					durationObject
 				);
+			} else if (tipo && !cantidad) {
+				if (tipo === "perma") {
+					let durationObject = "perma";
+					await ban(
+						interaction,
+						member,
+						motivo,
+						durationObject
+					);
+				} else {
+					return interaction.reply({
+						content: "Ingrese una cantidad de minutos/horas/días.",
+						ephemeral: true
+					});
+				}
+			} else if (!tipo && cantidad) {
+				return interaction.reply({
+					content: "Ingrese un tipo: minutos, horas o días.",
+					ephemeral: true
+				});
+			} else {
+				await ban(interaction, member, motivo);
 			}
 		}
 	}
